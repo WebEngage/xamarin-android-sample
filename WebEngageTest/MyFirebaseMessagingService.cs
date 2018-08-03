0246147b4889d8dea38d87be9fbcf1bb8b21b13c
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2018-08-03 17:56:41 +0530

    adds check if push message is from webengage

--- WebEngageTest/MyFirebaseMessagingService.cs
+++ WebEngageTest/MyFirebaseMessagingService.cs
@@ -19,8 +19,10 @@ namespace WebEngageTest
         public override void OnMessageReceived(RemoteMessage message)
         {
             base.OnMessageReceived(message);
-            Log.Debug("WebEngage", "Received push notification");
-            WebEngage.Get().Receive(message.Data);
+            IDictionary<string, string> data = message.Data;
+            if (data.ContainsKey("source") && "webengage".Equals(data["source"])) {
+                WebEngage.Get().Receive(data);
+            }
         }
     }
 }
